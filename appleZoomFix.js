4dc49d26a754c823e2a77dcacc19d8cef08d3f73
Cubo <55770955+MasterCubo@users.noreply.github.com>
2024-07-23 09:25:06 -0400

    Create appleZoomFix.js

---
+++ appleZoomFix.js
@@ -0,0 +1,8 @@
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
